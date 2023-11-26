472ac7450ba7b779a28e79b7fe3ed421ecb06379
Hummer82 <132070793+Hummer82@users.noreply.github.com>
2023-11-26 14:29:47 +0300

    1

--- nearbyshops/nearbyshops/settings.py
+++ nearbyshops/nearbyshops/settings.py
@@ -75,17 +75,27 @@ WSGI_APPLICATION = "nearbyshops.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.contrib.gis.db.backends.postgis",
+#         "NAME": "gis",
+#         "USER": "user001",
+#         "PASSWORD": "123456789",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.contrib.gis.db.backends.postgis",
-        "NAME": "gis",
-        "USER": "user001",
-        "PASSWORD": "123456789",
-        "HOST": "localhost",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hrdb',
+        'USER': 'postgres',
+        'PASSWORD': 'adminpg',
+        'HOST': '127.0.0.1',
+#        'PORT': '7371',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
